66e0a9d6986b03883ecbab7a819800707f7ec05d
johnchuma <johnvchuma@gmail.com>
2025-08-14 12:53:51 +0000

    fix: routes

--- modules/users/users.routes.js
+++ modules/users/users.routes.js
@@ -8,10 +8,6 @@ const {
   sendCode,
   getInfluencers,
   getMyInfo,
-<<<<<<< HEAD
-  getUser,
-=======
->>>>>>> 6a14771ad35720f413912ec4705c276a18916c1e
 } = require("./users.controllers");
 const { getPagination } = require("../../utils/getPagination");
 const router = Router();
@@ -21,13 +17,7 @@ router.post("/auth/confirm-code", confirmCode);
 router.post("/auth/send-code", sendCode);
 router.get("/influencers", getPagination, getInfluencers);
 router.get("/me", validateJWT, getMyInfo);
-<<<<<<< HEAD
-router.get("/:id",  getUser);
-router.patch("/:id",  updateUser);
-router.delete("/:id",  deleteUser);
-=======
 router.patch("/:id", validateJWT, updateUser);
 router.delete("/:id", deleteUser);
->>>>>>> 6a14771ad35720f413912ec4705c276a18916c1e
 
 module.exports = router;
